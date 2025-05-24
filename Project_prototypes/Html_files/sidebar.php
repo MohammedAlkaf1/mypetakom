5e3f68b4df5684dcde2aa017c7a06ea9168ac28c
aqel abdulaziz <ax0557115346@gamil.com>
2025-05-24 21:53:09 +0800

    advisor event page also side bar and header i put the event page so you can see it since it is related to yash and hamzah

---
+++ Project_prototypes/Html_files/sidebar.php
@@ -0,0 +1,10 @@
+
+<link rel="stylesheet" href="../Styles/sidebar.css">
+<aside class="sidebar">
+    <ul>
+        <li><a href="#">Profile</a></li>
+        <li><a href="./event_advisor.php">Events</a></li>
+        <li><a href="#">Manage Attendance</a></li>
+        <li><a href="#">Merit Applications</a></li>
+    </ul>
+</aside>
